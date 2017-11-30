8ee5ab3e51b2dda9375ee8fc0609515f945c39fa
Brad Hambrice <hambrice@uab.edu>
2017-11-30 04:39:47 +0000

    automatically backed up by learn

--- js/events.js
+++ js/events.js
@@ -12,7 +12,7 @@ function frameIt() {
   }
 
 function pressIt() {
-  $("input").on("keydown", function(key) {
+  $("form").on("keydown", function(key) {
     if (key.which == 67) {
       alert('g was pressed')
     }
