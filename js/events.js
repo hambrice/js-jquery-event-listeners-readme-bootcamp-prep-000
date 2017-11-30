01fe007efb91f4acbf24b7e3efa147bceae9e5a2
Brad Hambrice <hambrice@uab.edu>
2017-11-30 04:38:15 +0000

    automatically backed up by learn

--- js/events.js
+++ js/events.js
@@ -9,7 +9,12 @@ function frameIt() {
   $('img').on('load', function(){
     $('img').addClass("tasty")
   })
-  
+  }
+
+function pressIt() {
+  $("input").on("keydown", function(key) {
+    if (key.which == )
+  })
 }
 
 $(document).ready(function(){
